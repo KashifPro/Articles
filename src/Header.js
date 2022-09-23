492c84625b898e5e163f50e5d43ed464964b920e
Kashif <kas@gmail.com>
2022-09-23 22:06:02 +0500

    Public

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        
+    <nav className='navigation'>
+<div className='logo'>
+   <img className='logoImg' src='../logo3.png' alt='logo'></img>
+</div>
+
+<input type="checkbox" id="click"></input>
+         <label for="click" class="menu-btn">
+         <i class="fas fa-bars"></i>
+         </label>
+         
+    <ul>
+    
+        <li><a className='active' href='#'>Home</a></li>
+        <li><a href='#'>Services</a></li>
+        <li><a href='#'>Contact</a></li>
+        <li><a href='#'>Projects</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+         <form>
+            
+         </form>
+         <div>
+            using only HTML & CSS
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
